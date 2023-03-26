21a1048b3613d777ab10132769b4ca09c936f944
Egoist <epsilonelittr@gmail.com>
2023-03-26 07:01:46 +0300

    Add isBlocked and isAllowed methods

--- src/BlacklistWhitelist.php
+++ src/BlacklistWhitelist.php
@@ -8,11 +8,25 @@ use LaravelReady\BlacklistWhitelist\Enums\BlockType;
 
 class BlacklistWhitelist
 {
-    public static function subject(string $domain, BlockType $type): Model
+    public static function subject(string $subject, BlockType $type): Model
     {
         return Model::firstOrCreate([
-            'subject' => $domain,
+            'subject' => $subject,
             'type' => $type,
         ]);
+    }
+
+    public static function isBlocked(string $subject): bool
+    {
+        return Model::where('subject', $subject)
+            ->where('type', BlockType::Blacklist)
+            ->exists();
+    }
+
+    public static function isAllowed(string $subject): bool
+    {
+        return Model::where('subject', $subject)
+            ->where('type', BlockType::Whitelist)
+            ->exists();
     }
 }
